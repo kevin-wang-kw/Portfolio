ff31eaa81d9bec3d1ac988a85b9f11844acddb4e
Kevin Wang <234497635+kevin-wang-kw@users.noreply.github.com>
2025-11-19 16:27:07 -0500

    descriptions expanded, testing adding images to specific project pages

--- lib/data.ts
+++ lib/data.ts
@@ -9,19 +9,19 @@ export type Project = {
 export const StairAssist = {
   name: "Stair Assist",
   description:
-    "Capstone project creating a stair assist device for elderly client",
+    "Capstone project creating a stair assist device for elderly client to safely move up and down th",
   link: { href: "/stair-assist", label: "Read More" },
-  image: "/images/Stair Assist Iso.png", 
-  text: "This project was completed as my  year capstone to assist an elderly client in easing the movement \n up and down an outdoor set of stairs. The protype was designed to be suitable for various heights and\n stair surfaces. ",
-  
+  image: "/images/Stair Assist Iso.png",
+  text: "This project was completed as my  year capstone to assist an elderly client in easing the movement up and down an outdoor set of stairs. The protype was designed to be suitable for various heights and stair surfaces. ",
 };
 
 export const LensHolder = {
   name: "Lens Holder",
   description:
-    "Lens test fixture developed to assist production with testing",
+    "Lens test fixture developed to assist production with root cause analysis and life cycle testing.",
   link: { href: "/lens-holder", label: "Read More" },
   image: "/images/Iso View Lens Holder.JPG",
+  text: "The lens holder was designed to assist production their investigations. It is comprised of 2 main components the base plate with dove tail attachement points and variously sized lens holders that would slide into the exposed dove tails.",
 };
 
 export const Projects = [StairAssist, LensHolder];
