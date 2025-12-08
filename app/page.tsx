4256f9722fa3cf97ea469b1a55710042df615817
Kevin Wang <234497635+kevin-wang-kw@users.noreply.github.com>
2025-12-08 12:00:15 -0500

    main blurb typo corrections

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,11 @@ export default function Home() {
       </h1>
       <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
         I'm Kevin, a mechanical engineering graduate working towards earning my
-        P.Eng. With my degree in hand, I’m looking to dive into real-world
-        design, testing, and collaboration, eager to apply what I know while I
-        grow. I love turning sketches and ideas into tangible, working
-        solutions.
+        P.Eng. With my degree in hand, I'm looking to dive into real-world
+        design, testing, and collaboration. I love turning sketches and ideas
+        into tangible solutions even if there are speed bumps on the road
+        getting there. This page itself is an ongoing learning experience in
+        programing with Next.JS, getting more polished with each iteration.
       </p>
       <div className="mt-12">
         <h1 className="mb-2 text-xl font-semibold tracking-wide text-stone-700 uppercase">
@@ -38,10 +39,12 @@ export default function Home() {
                 />
               </div>
               <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
-                <Card.Link href={project.link.href} className="outline-none">{project.name}</Card.Link>
+                <Card.Link href={project.link.href} className="outline-none">
+                  {project.name}
+                </Card.Link>
               </h2>
               <Card.Description>{project.description}</Card.Description>
-              <div className="relative z-10 -mx-3 mt-6 flex rounded-full bg-zinc-100 px-3 py-2 text-sm font-medium text-zinc-500 transition group-hover:bg-teal-50 group-hover:text-teal-500 dark:text-zinc-200 group-focus-within:bg-teal-50 group-focus-within:text-teal-500">
+              <div className="relative z-10 -mx-3 mt-6 flex rounded-full bg-zinc-100 px-3 py-2 text-sm font-medium text-zinc-500 transition group-focus-within:bg-teal-50 group-focus-within:text-teal-500 group-hover:bg-teal-50 group-hover:text-teal-500 dark:text-zinc-200">
                 <LinkIcon className="h-6 w-6 flex-none" />
                 <span className="ml-2">{project.link.label}</span>
               </div>
